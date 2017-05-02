aebdbdb7c28aadb166a6f131a1d421e8f2c4a443
henrik <henrik@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2017-05-02 18:15:25 +0000

    more cout debugging
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7812 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_henrik_anneal/CSimAnneal.cpp
+++ ivp/src/lib_henrik_anneal/CSimAnneal.cpp
@@ -95,7 +95,8 @@ void CSimAnneal::addMeas(CMeasurement new_meas)
   // update energy
   double model = measModel(new_meas.t, new_meas.x, new_meas.y);
   Energy = sqrt((num_meas*pow(Energy,2) + pow(new_meas.temp - model,2))/(num_meas+1)); 
-  cout << ">>> Num_Meas=" << num_meas+1 << " Energy =" << Energy << endl;
+  cout << ">>> Num_Meas=" << num_meas+1 << " new_meas.temp=" << new_meas.temp 
+       << " model=" << model << " Energy =" << Energy << endl;
 }
 
 CMeasurement CSimAnneal::parseMeas(string report)
