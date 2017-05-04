95f5a239d80314878b644986769973249a35f02b
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2017-05-04 15:21:32 +0000

    added back pGradeFrontEstimate
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7815 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/pGradeFrontEstimate/GradeFrontEstimate_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME:                                                      */
+/*   ORGN: MIT Cambridge MA                                     */
+/*   FILE: GradeFrontEstimate_Info.h                            */
+/*   DATE: January 2012                                         */
+/****************************************************************/
+
+#ifndef GradeFrontEstimate_INFO_HEADER
+#define GradeFrontEstimate_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
