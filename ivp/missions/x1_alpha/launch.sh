2087c615adfc99b610d0b7245e0f68ed61917efc
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2017-07-31 17:08:51 +0000

    x1_alpha
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7865 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/x1_alpha/launch.sh
+++ ivp/missions/x1_alpha/launch.sh
@@ -25,9 +25,10 @@ done
 printf "Launching the %s MOOS Community (WARP=%s) \n"  $COMMUNITY $TIME_WARP
 pAntler $COMMUNITY.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
 
-uMAC $COMMUNITY.moos
+uMAC -t $COMMUNITY.moos
 
 printf "Killing all processes ... \n"
 kill %1 
+mykill
 printf "Done killing processes.   \n"
 
