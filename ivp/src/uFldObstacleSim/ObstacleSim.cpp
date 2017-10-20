85b5cdeabfd7c11b5746340410cd97e4288ce5c6
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2017-10-20 15:04:14 +0000

    initial version of uFldObstacleSim
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7909 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/uFldObstacleSim/ObstacleSim.cpp
@@ -0,0 +1,226 @@
+/*****************************************************************/
+/*    NAME: Michael R. Benjamin                                  */
+/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA     */
+/*    FILE: ObstacleSim.cpp                                      */
+/*    DATE: October 19th, 2017                                   */
+/*****************************************************************/
+
+#include <iterator>
+#include "MBUtils.h"
+#include "ACTable.h"
+#include "ObstacleSim.h"
+#include "FileBuffer.h"
+#include "ColorParse.h"
+#include "XYFormatUtilsPoly.h"
+
+using namespace std;
+
+//---------------------------------------------------------
+// Constructor
+
+ObstacleSim::ObstacleSim()
+{
+  m_viewables_posted = false;
+
+  m_min_range = 0;
+  m_min_poly_size = 0;
+  m_max_poly_size = 0;
+
+  m_poly_fill_color  = "white";
+  m_poly_edge_color  = "gray50";
+  m_poly_vert_color  = "gray50";
+  m_poly_label_color = "invisible";
+
+  m_poly_transparency = 0.15;
+  m_poly_edge_size   = 1;
+  m_poly_vert_size   = 1;
+}
+
+//---------------------------------------------------------
+// Procedure: OnNewMail
+
+bool ObstacleSim::OnNewMail(MOOSMSG_LIST &NewMail)
+{
+  AppCastingMOOSApp::OnNewMail(NewMail);
+
+  MOOSMSG_LIST::iterator p;
+  for(p=NewMail.begin(); p!=NewMail.end(); p++) {
+    CMOOSMsg &msg = *p;
+    string key    = msg.GetKey();
+
+#if 0 // Keep these around just for template
+    string comm  = msg.GetCommunity();
+    double dval  = msg.GetDouble();
+    string sval  = msg.GetString(); 
+    string msrc  = msg.GetSource();
+    double mtime = msg.GetTime();
+    bool   mdbl  = msg.IsDouble();
+    bool   mstr  = msg.IsString();
+#endif
+    
+    if(key == "FOO") 
+      cout << "great!";
+    
+    else if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
+      reportRunWarning("Unhandled Mail: " + key);
+  }
+	
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnConnectToServer
+
+bool ObstacleSim::OnConnectToServer()
+{
+   registerVariables();
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: Iterate()
+
+bool ObstacleSim::Iterate()
+{
+  AppCastingMOOSApp::Iterate();
+
+  // Do your thing here!
+  if(!m_viewables_posted) {
+    postViewableObstacles();
+    m_viewables_posted = true;
+  }
+
+  AppCastingMOOSApp::PostReport();
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnStartUp()
+
+bool ObstacleSim::OnStartUp()
+{
+  AppCastingMOOSApp::OnStartUp();
+
+  STRING_LIST sParams;
+  m_MissionReader.EnableVerbatimQuoting(false);
+  if(!m_MissionReader.GetConfiguration(GetAppName(), sParams))
+    reportConfigWarning("No config block found for " + GetAppName());
+
+  STRING_LIST::iterator p;
+  for(p=sParams.begin(); p!=sParams.end(); p++) {
+    string orig  = *p;
+    string line  = *p;
+    string param = tolower(biteStringX(line, '='));
+    string value = line;
+
+    bool handled = false;
+    if(param == "obstacle_file") 
+      handled = processObstacleFile(value);
+    else if((param == "poly_vert_color") && isColor(value))
+      handled = setColorOnString(m_poly_vert_color, value);
+    else if((param == "poly_fill_color") && isColor(value))
+      handled = setColorOnString(m_poly_fill_color, value);
+    else if((param == "poly_edge_color") && isColor(value))
+      handled = setColorOnString(m_poly_edge_color, value);
+    else if((param == "poly_label_color") && isColor(value))
+      handled = setColorOnString(m_poly_label_color, value);
+
+    if(!handled)
+      reportUnhandledConfigWarning(orig);
+  }
+  
+  registerVariables();	
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: registerVariables
+
+void ObstacleSim::registerVariables()
+{
+  AppCastingMOOSApp::RegisterVariables();
+  // Register("FOOBAR", 0);
+}
+
+
+//------------------------------------------------------------
+// Procedure: processObstacleFile
+
+bool ObstacleSim::processObstacleFile(string filename)
+{
+  vector<string> lines = fileBuffer(filename);
+  unsigned int i, vsize = lines.size();
+  if(vsize == 0) {
+    reportConfigWarning("Error reading: " + filename);
+    return(false);
+  }
+
+  for(i=0; i<vsize; i++) {
+    string line = stripBlankEnds(lines[i]);
+    if((line == "") || strBegins(line, "//"))
+      continue;
+    
+    string left  = biteStringX(line, '=');
+    string right = line;
+    if(left == "region") {
+      XYPolygon region = string2Poly(right);
+      if(!region.is_convex()) {
+	reportConfigWarning("Poorly specified region: " + right);
+	return(false);
+      }
+      m_poly_region = region;
+    }
+    else if(left == "poly") {
+      XYPolygon poly = string2Poly(right);
+      if(!poly.is_convex()) {
+	reportConfigWarning("Poorly specified obstacle: " + right);
+	return(false);
+      }
+      poly.set_color("edge", m_poly_edge_color);
+      poly.set_color("vertex", m_poly_vert_color);
+      poly.set_color("fill", m_poly_fill_color);
+      poly.set_color("label", m_poly_label_color);
+      poly.set_vertex_size(m_poly_vert_size);
+      poly.set_edge_size(m_poly_edge_size);
+      poly.set_transparency(m_poly_transparency);
+      m_obstacles.push_back(poly);
+    }
+  }
+  return(true);
+}
+
+//------------------------------------------------------------
+// Procedure: postViewableObstacles()
+
+void ObstacleSim::postViewableObstacles()
+{
+  for(unsigned int i=0; i<m_obstacles.size(); i++) {
+    string spec = m_obstacles[i].get_spec();
+    Notify("VIEW_POLYGON", spec);
+  }
+
+  if(m_poly_region.is_convex())
+    Notify("VIEW_POLYGON", m_poly_region.get_spec());
+}
+
+//------------------------------------------------------------
+// Procedure: buildReport()
+
+bool ObstacleSim::buildReport() 
+{
+  m_msgs << "============================================" << endl;
+  m_msgs << "Obstacles: " << uintToString(m_obstacles.size()) << endl;
+  m_msgs << "MinRange: " << doubleToString(m_min_range) << endl;
+
+  ACTable actab(4);
+  actab << "Alpha | Bravo | Charlie | Delta";
+  actab.addHeaderLines();
+  actab << "one" << "two" << "three" << "four";
+  m_msgs << actab.getFormattedString();
+
+  return(true);
+}
+
+
+
+
---
+++ ivp/src/uFldObstacleSim/ObstacleSim.h
@@ -0,0 +1,59 @@
+/*****************************************************************/
+/*    NAME: Michael R. Benjamin                                  */
+/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA     */
+/*    FILE: ObstacleSim.h                                        */
+/*    DATE: October 19th, 2017                                   */
+/*****************************************************************/
+
+#ifndef OBSTACLESIM_HEADER
+#define OBSTACLESIM_HEADER
+
+#include <string>
+#include <vector>
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include "XYPolygon.h"
+
+class ObstacleSim : public AppCastingMOOSApp
+{
+ public:
+  ObstacleSim();
+  virtual ~ObstacleSim() {};
+  
+ protected: // Standard MOOSApp functions to overload  
+  bool OnNewMail(MOOSMSG_LIST &NewMail);
+  bool Iterate();
+  bool OnConnectToServer();
+  bool OnStartUp();
+  
+ protected: // Standard AppCastingMOOSApp function to overload 
+  bool buildReport();
+  
+ protected:
+  void registerVariables();
+  bool processObstacleFile(std::string filename);
+  void postViewableObstacles();
+  
+ private: // Configuration variables
+
+  std::vector<XYPolygon> m_obstacles;
+
+  XYPolygon m_poly_region;
+  double    m_min_range;
+  double    m_min_poly_size;
+  double    m_max_poly_size;
+
+  std::string m_poly_fill_color;
+  std::string m_poly_edge_color;
+  std::string m_poly_vert_color;
+  std::string m_poly_label_color;
+
+  double    m_poly_edge_size;
+  double    m_poly_vert_size;
+  double    m_poly_transparency;
+  
+ private: // State variables
+
+  bool m_viewables_posted;
+};
+
+#endif 
