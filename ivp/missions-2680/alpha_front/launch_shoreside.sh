e49b7b9b774ec7f2039acfa10f677099faf35e10
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2019-05-16 12:07:01 +0000

    mods to example missions in ivp/missions
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@8563 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions-2680/alpha_front/launch_shoreside.sh
+++ ivp/missions-2680/alpha_front/launch_shoreside.sh
@@ -13,6 +13,7 @@ JUST_MAKE="no"
 SHORE_IPADDR="localhost"
 SHORE_PSHARE="9200"
 SHORE_MOOSDB="9000"
+CTD="one"
 
 #---------------------------------------------------------
 #  Part 2: Check for and handle command-line arguments
@@ -29,6 +30,10 @@ for ARGI; do
         TIME_WARP=$ARGI
     elif [ "${ARGI}" = "--just_make" -o "${ARGI}" = "-j" ] ; then
         JUST_MAKE="yes"
+    elif [ "${ARGI}" = "-c2" ] ; then
+        CTD="two"
+    elif [ "${ARGI}" = "-c3" ] ; then
+        CTD="three"
     elif [ "${ARGI:0:11}" = "--shoreip=" ] ; then
         SHORE_IPADDR="${ARGI#--shoreip=*}"
     elif [ "${ARGI:0:13}" = "--shoreps=" ] ; then
@@ -46,8 +51,8 @@ done
 #    Note: Undef macros will be aletered to user with nsplug -i flag
 #---------------------------------------------------------
 nsplug meta_shoreside.moos targ_shoreside.moos -i -f WARP=$TIME_WARP \
-       SHORE_PSHARE=$SHORE_PSHARE  SHORE_MOOSDB=$SHORE_MOOSDB         \
-       SHORE_IPADDR=$SHORE_IPADDR  
+       SHORE_PSHARE=$SHORE_PSHARE  SHORE_MOOSDB=$SHORE_MOOSDB        \
+       SHORE_IPADDR=$SHORE_IPADDR  CTD=$CTD
 
 #---------------------------------------------------------
 #  Part 4: Possibly exit now if we're just building targ files
