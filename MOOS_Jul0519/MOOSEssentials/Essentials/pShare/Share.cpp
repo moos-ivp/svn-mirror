a47b230f2f280c6c6ffb22cba5b947c48049372e
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2019-07-06 00:50:16 +0000

    mods to example missions in ivp/missions
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@8622 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- MOOS_Jul0519/MOOSEssentials/Essentials/pShare/Share.cpp
+++ MOOS_Jul0519/MOOSEssentials/Essentials/pShare/Share.cpp
@@ -17,7 +17,7 @@
 
 #include "MOOS/libMOOS/Utils/MOOSUtilityFunctions.h"
 #include "MOOS/libMOOS/Utils/IPV4Address.h"
-#include "MOOS/libMOOS/Thirdparty/getpot/GetPot"
+//#include "MOOS/libMOOS/Thirdparty/getpot/GetPot"
 #include "MOOS/libMOOS/Utils/SafeList.h"
 #include "MOOS/libMOOS/Utils/ConsoleColours.h"
 #include "MOOS/libMOOS/Utils/KeyboardCapture.h"
