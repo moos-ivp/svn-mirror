18795a91dbaf97c9c7a22454cbb538f065616ada
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2019-08-06 14:41:29 +0000

    edit to pObstacleMgr to provide full bhv name on resolved alerts
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@8718 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/pObstacleMgr/ObstacleManager.cpp
+++ ivp/src/pObstacleMgr/ObstacleManager.cpp
@@ -688,7 +688,8 @@ void ObstacleManager::manageMemory()
     m_map_updates_total.erase(key);
     m_clusters_released++;
 
-    string done_str = "name=" + key + "#resolved=true";
+    //string done_str = "name=" + key + "#resolved=true";
+    string done_str = "name=" + m_alert_name + key + "#resolved=true";
     Notify(m_alert_var, done_str);
     
     // If a poly is being freed, first post a viewable poly with
