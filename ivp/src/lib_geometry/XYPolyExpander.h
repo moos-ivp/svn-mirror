50438b30248a23bbeed61d3f295c36470d44d42d
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2019-10-01 19:45:57 +0000

    Added XYPolyExpander to geometry lib, and required new add_vertex_delta function tto the XYPolygon class
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@8797 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/lib_geometry/XYPolyExpander.h
@@ -0,0 +1,75 @@
+/*****************************************************************/
+/*    NAME: Michael Benjamin                                     */
+/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA     */
+/*    FILE: XYPolyExpander.h                                     */
+/*    DATE: Sep 7th, 2019                                        */
+/*                                                               */
+/* This file is part of IvP Helm Core Libs                       */
+/*                                                               */
+/* IvP Helm Core Libs is free software: you can redistribute it  */
+/* and/or modify it under the terms of the Lesser GNU General    */
+/* Public License as published by the Free Software Foundation,  */
+/* either version 3 of the License, or (at your option) any      */
+/* later version.                                                */
+/*                                                               */
+/* IvP Helm Core Libs is distributed in the hope that it will    */
+/* be useful but WITHOUT ANY WARRANTY; without even the implied  */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the Lesser GNU General Public License for more   */
+/* details.                                                      */
+/*                                                               */
+/* You should have received a copy of the Lesser GNU General     */
+/* Public License along with MOOS-IvP.  If not, see              */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
+ 
+#ifndef XY_POLYGON_EXPANDER_HEADER
+#define XY_POLYGON_EXPANDER_HEADER
+
+#include <vector>
+#include <string>
+#include "XYPolygon.h"
+
+class XYPolyExpander {
+public:
+  XYPolyExpander();
+  ~XYPolyExpander() {}
+
+  bool      setPoly(XYPolygon poly);
+  void      setDegreeDelta(double);
+  void      setVertexProximityThresh(double);
+  
+  XYPolygon getBufferPoly(double buff);
+  
+protected:
+  bool      expandSegments();
+  bool      buildCorners();
+  bool      buildNewPoly();
+  void      clear();
+  
+private: // Config variables
+  
+  XYPolygon m_poly_orig;
+  XYPolygon m_poly_buff;
+
+  double    m_buff;
+  double    m_deg_delta;
+  double    m_vertex_proximity_thresh;
+
+private: // State variables
+
+  // Variables for holding the temporary edge vertices
+  // Index aligns with indices of poly_orig
+  std::vector<double> m_px1;
+  std::vector<double> m_py1;
+  std::vector<double> m_px2;
+  std::vector<double> m_py2;
+  
+  // Variables for holding the temporary corner vertices
+  // Outer Index aligns with indices of poly_orig
+  std::vector<std::vector<double> > m_ipx;
+  std::vector<std::vector<double> > m_ipy;
+};
+
+#endif
+
