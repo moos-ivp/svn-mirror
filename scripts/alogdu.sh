776425d0da674505bfba9df650dc115315d77948
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2019-11-29 22:59:28 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@8882 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- scripts/alogdu.sh
+++ scripts/alogdu.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#--------------------------------------------------------------
+#  Script: alogdu.sh
+#  Author: Michael Benjamin
+#  Date:   November 2018
+#  About:  
+#--------------------------------------------------------------
 
 find . -name '*.alog'  -print -exec ls -lh {} \;
 
