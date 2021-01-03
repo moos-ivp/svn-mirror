51a2e01792f9c517eb329682d98710460acf91c3
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2021-01-03 02:18:21 +0000

    added include cstdlib for an atof call in  WatchCast.cpp
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@9518 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_realm/WatchCast.cpp
+++ ivp/src/lib_realm/WatchCast.cpp
@@ -21,6 +21,7 @@
 /* <http://www.gnu.org/licenses/>.                               */
 /*****************************************************************/
 
+#include <cstdlib>
 #include "MBUtils.h"
 #include "WatchCast.h"
 
