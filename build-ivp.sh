08b16f30411663ee0026a1a6dce050fd9270b5ab
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2021-01-23 02:43:56 +0000

    minor fix to build-ivp.sh
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@9527 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- build-ivp.sh
+++ build-ivp.sh
@@ -1,61 +1,56 @@
 #!/bin/bash 
+#---------------------------------------------------------
+# Script: build-ivp.sh
+#     By: Mike Benjamin
+#---------------------------------------------------------
+#  Part 1: Define global script variables
+#---------------------------------------------------------
 
 BUILD_DEBUG="yes"  
 BUILD_OPTIM="yes"
 CLEAN="no"
 CMD_ARGS="-j$(getconf _NPROCESSORS_ONLN)"
-BUILD_GUI_CODE="ON"
 BUILD_WITH_UTM="-DUSE_UTM=ON"
 
 # By default, all code is built
-# On Raspbian, by default, only min-robot code is built
 BUILD_BOT_CODE_ONLY="OFF"
-LSB_RELEASE=`which lsb_release`
-if [ "$LSB_RELEASE" != "" ]; then
-    OS_INFO=`lsb_release -i -s`
-    if [ "${OS_INFO}" = "Raspbian" ]; then
-	BUILD_BOT_CODE_ONLY="ON"
-	BUILD_GUI_CODE="OFF"
-    fi
-fi
+BUILD_GUI_CODE="ON"
 
-print_usage_and_exit()
-{
-    echo "build-ivp.sh [OPTIONS] [MAKE ARGS]            "
-    echo "Options:                                      "
-    echo "  --help, -h                                  "
-    echo "  --nodebug                                   "
-    echo "    Do not include the -g compiler flag       "
-    echo "  --noopt                                     "
-    echo "    Do not include the -Os compiler flag      "
-    echo "  --fast, -f                                  "
-    echo "    Do not include the -Os, -g compiler flags "
-    echo "  --nogui, -n                                 "
-    echo "    Do not build GUI related apps             "
-    echo "  --utm_off, -u                               "
-    echo "    Do not build wit UTM for Geodesy          "
-    echo "  --minrobot, -m                              "
-    echo "    Only build minimal robot apps             "
-    echo "    (Even smaller subset than with --nogui)   "
-    echo "  --minrobotx, -mx                            "
-    echo "    Override min-robot default on Raspbian    "
-    echo "  --clean, -c                                 "
-    echo "    Invokes make clean and removes build/*    "
-    echo "                                              "
-    echo "By default, all code is built, and the debug and   "
-    echo "optimization compiler flags are invoked.           "
-    echo "                                                   "
-    echo "Note:                                              "
-    echo "  By default -jN is provided to make to utilize up to N    "
-    echo "  processors in the build. This can be overridden simply   "
-    echo "  by using -j1 on the command line instead. This will give "
-    echo "  more reasonable output if there should be a build error. "
-    exit 1
-}
-
+#---------------------------------------------------------
+# Part 2: Handle Command Line Arguments
+#---------------------------------------------------------
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ]; then
-        print_usage_and_exit;
+	echo "build-ivp.sh [OPTIONS] [MAKE ARGS]            "
+	echo "Options:                                      "
+	echo "  --help, -h                                  "
+	echo "  --nodebug                                   "
+	echo "    Do not include the -g compiler flag       "
+	echo "  --noopt                                     "
+	echo "    Do not include the -Os compiler flag      "
+	echo "  --fast, -f                                  "
+	echo "    Do not include the -Os, -g compiler flags "
+	echo "  --nogui, -n                                 "
+	echo "    Do not build GUI related apps             "
+	echo "  --utm_off, -u                               "
+	echo "    Do not build wit UTM for Geodesy          "
+	echo "  --minrobot, -m                              "
+	echo "    Only build minimal robot apps             "
+	echo "    (Even smaller subset than with --nogui)   "
+	echo "  --minrobotx, -mx                            "
+	echo "    Override min-robot default on Raspbian    "
+	echo "  --clean, -c                                 "
+	echo "    Invokes make clean and removes build/*    "
+	echo "                                              "
+	echo "By default, all code is built, and the debug and   "
+	echo "optimization compiler flags are invoked.           "
+	echo "                                                   "
+	echo "Note:                                              "
+	echo "  By default -jN is provided to make to utilize up to N    "
+	echo "  processors in the build. This can be overridden simply   "
+	echo "  by using -j1 on the command line instead. This will give "
+	echo "  more reasonable output if there should be a build error. "
+	exit 0
     elif [ "${ARGI}" = "--nodebug" ]; then
         BUILD_DEBUG="no"
     elif [ "${ARGI}" = "--noopt" ]; then
@@ -84,8 +79,25 @@ for ARGI; do
     fi
 done
 
-#########################################################################
-CMAKE_CXX_FLAGS="-Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -pedantic -fPIC"
+#---------------------------------------------------------
+# Part 3: On Raspbian, by default, only min-robot code is built
+#---------------------------------------------------------
+LSB_RELEASE=`which lsb_release`
+if [ "$LSB_RELEASE" != "" ]; then
+    OS_INFO=`lsb_release -i -s`
+    if [ "${OS_INFO}" = "Raspbian" ]; then
+	BUILD_BOT_CODE_ONLY="ON"
+	BUILD_GUI_CODE="OFF"
+    fi
+fi
+
+
+#---------------------------------------------------------
+# Part 4: Set Compiler flags
+#---------------------------------------------------------
+CMAKE_CXX_FLAGS="-Wall -Wextra -Wno-unused-parameter "
+CMAKE_CXX_FLAGS+="-Wno-missing-field-initializers -pedantic -fPIC "
+
 if [ "${BUILD_DEBUG}" = "yes" ] ; then
     CMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS" -g"
 fi
@@ -96,7 +108,9 @@ fi
 echo "Compiler flags: ${CMAKE_CXX_FLAGS}  " 
 
 
-#########################################################################
+#---------------------------------------------------------
+# Part 5
+#---------------------------------------------------------
 INVOC_ABS_DIR="$(pwd)"
 SCRIPT_ABS_DIR="$(cd $(dirname "$0") && pwd -P)"
 
@@ -108,7 +122,7 @@ SRC_ABS_DIR="${SCRIPT_ABS_DIR}/ivp/src"
 echo "Built files will be placed into these directories: "
 echo "  Intermediate build files: ${BLD_ABS_DIR}         "
 echo "  Libraries:                ${LIB_ABS_DIR}         "
-echo "  Programs:                 ${BIN_ABS_DIR}       \n"
+echo "  Programs:                 ${BIN_ABS_DIR}         "
 
 mkdir -p "${BLD_ABS_DIR}"
 mkdir -p "${LIB_ABS_DIR}"
@@ -116,19 +130,23 @@ mkdir -p "${BIN_ABS_DIR}"
 
 cd "${BLD_ABS_DIR}"
 
-########################################################################
-# For back compatability, if user has environment variable
-# IVP_BUILD_GUI_CODE set to "OFF" then honor it here as if --nogui
-# were set on the command line
+#---------------------------------------------------------
+# Part 6: For back compatability, if user has environment var
+#         IVP_BUILD_GUI_CODE set to "OFF" then honor it here
+#         as if --nogui it were set on the command line
+#---------------------------------------------------------
 
 if [ "${IVP_BUILD_GUI_CODE}" = "OFF" ] ; then
     BUILD_GUI_CODE="OFF"
-    echo "IVP GUI Apps will not be built. IVP_BUILD_GUI_CODE env var is OFF"
+    echo "IVP GUI Apps will not be built since the   "
+    echo "IVP_BUILD_GUI_CODE environment var is OFF  "
 fi
 
 echo "BUILD_GUI_CODE = ${BUILD_GUI_CODE} "
 
-########################################################################
+#---------------------------------------------------------
+# Part 7: Invoke CMake
+#---------------------------------------------------------
 echo "Invoking cmake..."
 
 echo "BUILD_BOT_CODE_ONLY: ${BUILD_BOT_CODE_ONLY}   "
@@ -142,7 +160,9 @@ cmake -DIVP_BUILD_GUI_CODE=${BUILD_GUI_CODE}               \
       ${IVP_CMAKE_FLAGS}                                   \
       "${SRC_ABS_DIR}"
 
-########################################################################
+#---------------------------------------------------------
+# Part 8: Invoke Make
+#---------------------------------------------------------
 echo "Invoking make ${CMD_ARGS}"
 
 RESULT=0
