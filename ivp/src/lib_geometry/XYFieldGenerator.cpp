900441bc32ff5929257010b23a2c84b0d707f9d3
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2021-09-14 12:53:29 +0000

    generalized XYFieldGenerator
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@9833 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_geometry/XYFieldGenerator.cpp
+++ ivp/src/lib_geometry/XYFieldGenerator.cpp
@@ -27,6 +27,7 @@
 #include <cstdlib>
 #include "XYFieldGenerator.h"
 #include "MBUtils.h"
+#include "GeomUtils.h"
 
 using namespace std;
 
@@ -36,10 +37,16 @@ using namespace std;
 XYFieldGenerator::XYFieldGenerator()
 {
   m_snap = 0.1;
+  m_max_tries = 10000;
+  m_targ_amt = 10;
+  
+  // A non-positive buffer dist disables this feature
+  m_buffer_dist = 0;
+  srand(time(NULL));
 }
 
 //---------------------------------------------------------
-// Procedure: addPolygon
+// Procedure: addPolygon()
 
 bool XYFieldGenerator::addPolygon(string spec)
 {
@@ -49,7 +56,7 @@ bool XYFieldGenerator::addPolygon(string spec)
 }
 
 //---------------------------------------------------------
-// Procedure: addPolygon
+// Procedure: addPolygon()
 
 bool XYFieldGenerator::addPolygon(XYPolygon poly)
 {
@@ -96,9 +103,16 @@ bool XYFieldGenerator::setSnap(double snap_val)
   return(true);
 }
   
-
 //---------------------------------------------------------
-// Procedure: getPolygon
+// Procedure: clear()
+
+void XYFieldGenerator::clear()
+{
+  m_vpoints.clear();
+}
+  
+//---------------------------------------------------------
+// Procedure: getPolygon()
 
 XYPolygon XYFieldGenerator::getPolygon(unsigned int ix)
 {
@@ -112,41 +126,179 @@ XYPolygon XYFieldGenerator::getPolygon(unsigned int ix)
   
 
 //---------------------------------------------------------
-// Procedure: generatePoint
+// Procedure: generatePoint()
+//   Purpose: Generate a new XYPoint that resides one of the
+//            field regions.
+//            No check made here for buffer distance.
 
 XYPoint XYFieldGenerator::generatePoint()
 {
   XYPoint new_point;
-
+  if(m_polygons.size() == 0)
+    return(new_point);
+  
   double xlen = m_poly_max_x - m_poly_min_x;
   double ylen = m_poly_max_y - m_poly_min_y;
 
   if((xlen <= 0) || (ylen <= 0))
     return(new_point);
 
-  bool         contained = false;
-  unsigned int max_tries = 1000;
-  
-  double rand_x, rand_y;
-  
-  while(!contained && (max_tries > 0)) {
+  for(unsigned int i=0; i<m_max_tries; i++) {
     int    rand_int_x = rand() % 10000;
     int    rand_int_y = rand() % 10000;
     double rand_pct_x = (double)(rand_int_x) / 10000;
     double rand_pct_y = (double)(rand_int_y) / 10000;
     
+    double rand_x, rand_y;
     rand_x = snapToStep(m_poly_min_x + (rand_pct_x * xlen), m_snap);
     rand_y = snapToStep(m_poly_min_y + (rand_pct_y * ylen), m_snap);
-    
-    unsigned int j, jsize = m_polygons.size();
-    for(j=0; j<jsize; j++) 
-      if(m_polygons[j].contains(rand_x, rand_y))
-	contained = true;
+
+    if(isPointInRegion(rand_x, rand_y)) {
+      new_point.set_vx(rand_x);
+      new_point.set_vy(rand_y);
+      return(new_point);
+    }
   }
   
-  if(contained) {
-    new_point.set_vx(rand_x);
-    new_point.set_vy(rand_y);
-  }
+  // If failed after max_tries, return point at center of
+  // first polygon.
+  new_point.set_vx(m_polygons[0].get_center_x());
+  new_point.set_vy(m_polygons[0].get_center_y());
   return(new_point);
 }
+
+
+//---------------------------------------------------------
+// Procedure: addPoint(const XYPoint&)
+//            Convenience function
+
+bool XYFieldGenerator::addPoint(const XYPoint& point)
+{
+  return(addPoint(point.x(), point.y()));
+}
+
+//---------------------------------------------------------
+// Procedure: getNewestPoint()
+
+XYPoint XYFieldGenerator::getNewestPoint() const
+{
+  XYPoint null_pt;
+  if(m_vpoints.size() == 0)
+    return(null_pt);
+
+  return(m_vpoints.back());
+}
+
+//---------------------------------------------------------
+// Procedure: addPoint(double vx, double vy)
+//   Purpose: If point is in one of the field regions, add
+//              it to the vector of points.
+//            If point is too close to one of the other
+//            points, search for nearby points that do not
+//              violate the buffer distance requirement.
+//   Returns: true if the point or a neighbor was added.
+//            false otherwise.
+
+bool XYFieldGenerator::addPoint(double vx, double vy)
+{
+  if(m_polygons.size() == 0)
+    return(false); 
+  if(!isPointInRegion(vx, vy))
+    return(false);
+
+  XYPoint new_point(vx, vy);
+  if(!isPointTooClose(new_point)) {
+    m_vpoints.push_back(new_point);
+    return(true);
+  }
+
+  double adjustable_dist = m_buffer_dist + 1;
+ 
+  for(unsigned int tries=0; tries<m_max_tries; tries++) {
+    int rand_ang = rand() % 360;
+    double tx = vx;
+    double ty = vy;
+    
+    if(tries != 0)
+      projectPoint(rand_ang, adjustable_dist, vx, vy, tx, ty); 
+
+    if(isPointInRegion(tx, ty) && !isPointTooClose(tx, ty)) {
+      XYPoint new_point(tx, ty);
+      new_point.set_label("P" + uintToString(m_vpoints.size()));
+      m_vpoints.push_back(new_point);
+      return(true);
+    }
+    if((tries % 10) == 0)
+      adjustable_dist += 1;
+  }
+  
+  return(false);
+}
+
+//---------------------------------------------------------
+// Procedure: generatePoints()
+
+void XYFieldGenerator::generatePoints(unsigned int amt)
+{
+  if(amt == 0)
+    amt = m_targ_amt;
+
+  m_vpoints.clear();
+
+  for(unsigned int i=0; i<amt; i++) {
+    XYPoint new_point = generatePoint();
+    addPoint(new_point);
+  }
+}
+
+//---------------------------------------------------------
+// Procedure: isPointTooClose()
+
+bool XYFieldGenerator::isPointTooClose(const XYPoint& point)
+{
+  for(unsigned int i=0; i<m_vpoints.size(); i++) {
+    if(distPointToPoint(point, m_vpoints[i]) < m_buffer_dist)
+      return(true);
+  }
+  
+  return(false);
+}
+
+//---------------------------------------------------------
+// Procedure: isPointTooClose()
+
+bool XYFieldGenerator::isPointTooClose(double vx, double vy)
+{
+  XYPoint point(vx, vy);
+  for(unsigned int i=0; i<m_vpoints.size(); i++) {
+    if(distPointToPoint(point, m_vpoints[i]) < m_buffer_dist)
+      return(true);
+  }
+  
+  return(false);
+}
+
+//---------------------------------------------------------
+// Procedure: isPointInRegion()
+
+bool XYFieldGenerator::isPointInRegion(const XYPoint& point)
+{
+  for(unsigned int j=0; j<m_polygons.size(); j++) 
+    if(m_polygons[j].contains(point.x(), point.y()))
+      return(true);
+  
+  return(false);
+}
+
+//---------------------------------------------------------
+// Procedure: isPointInRegion()
+
+bool XYFieldGenerator::isPointInRegion(double vx, double vy)
+{
+  for(unsigned int j=0; j<m_polygons.size(); j++) 
+    if(m_polygons[j].contains(vx, vy))
+      return(true);
+  
+  return(false);
+}
+
--- ivp/src/lib_geometry/XYFieldGenerator.h
+++ ivp/src/lib_geometry/XYFieldGenerator.h
@@ -40,23 +40,48 @@ class XYFieldGenerator
   bool addPolygon(std::string);
   bool addPolygon(XYPolygon);
   bool setSnap(double);
+  void setBufferDist(double v) {m_buffer_dist=v;}
+  void setMaxTries(unsigned int v) {m_max_tries=v;}
+  void setTargAmt(unsigned int v) {m_targ_amt=v;}
+
+  void clear();
   
   XYPoint generatePoint();
+  XYPoint generatePoint(double vx, double vy);
+  void    generatePoints(unsigned int amt=0);
+  bool    addPoint(const XYPoint&);
+  bool    addPoint(double vx, double vy);
 
+  XYPoint getNewestPoint() const;
+  
+  std::vector<XYPoint> getPoints() {return(m_vpoints);}
+    
   unsigned int polygonCount() {return(m_polygons.size());}
   unsigned int size()         {return(m_polygons.size());}
   XYPolygon    getPolygon(unsigned int);
 
  protected: // Config variables
-  std::vector<XYPolygon>    m_polygons;
+  bool isPointTooClose(const XYPoint&);
+  bool isPointTooClose(double vx, double vy);
+  bool isPointInRegion(const XYPoint&);
+  bool isPointInRegion(double vx, double vy);
+  
+ protected: // Config variables
+  std::vector<XYPolygon> m_polygons;
 
+  unsigned int m_max_tries;
+  unsigned int m_targ_amt;
+  
+  double   m_buffer_dist;
   double   m_snap;
-
+  
  protected: // State variables
   double   m_poly_min_x;
   double   m_poly_min_y;
   double   m_poly_max_x;
   double   m_poly_max_y;
+
+  std::vector<XYPoint> m_vpoints;
 };
 
 #endif 
