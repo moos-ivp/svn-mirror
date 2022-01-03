057599acc4aa8b3a415367c3c4f853c9f31c8f60
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2022-01-03 20:00:53 +0000

    added OpenURL utility to mbutil
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@9923 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/lib_mbutil/OpenURL.cpp
@@ -0,0 +1,56 @@
+/*****************************************************************/
+/*    NAME: Michael Benjamin                                     */
+/*    ORGN: Dept of Mechanical Eng / MIT Cambridge MA            */
+/*    FILE: OpenURL.cpp                                          */
+/*    DATE: Jan 23rd, 2022                                       */
+/*                                                               */
+/* This file is part of IvP Helm Core Libs                       */
+/*                                                               */
+/* IvP Helm Core Libs is free software: you can redistribute it  */
+/* and/or modify it under the terms of the Lesser GNU General    */
+/* Public License as published by the Free Software Foundation,  */
+/* either version 3 of the License, or (at your option) any      */
+/* later version.                                                */
+/*                                                               */
+/* IvP Helm Core Libs is distributed in the hope that it will    */
+/* be useful but WITHOUT ANY WARRANTY; without even the implied  */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the Lesser GNU General Public License for more   */
+/* details.                                                      */
+/*                                                               */
+/* You should have received a copy of the Lesser GNU General     */
+/* Public License along with MOOS-IvP.  If not, see              */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
+
+#include <cstdlib>
+#include <iostream>
+#include "OpenURL.h"
+#include "MBUtils.h"
+
+#ifdef UNIX
+  #define OPEN_URL_FUNCTION "xdg-open "
+#elif WIN32
+  #define OPEN_URL_FUNCTION "xdg-open "
+#else
+  #define OPEN_URL_FUNCTION "open "
+#endif
+
+using namespace std;
+
+void openURL(string url)
+{
+  if(!strBegins(url, "http"))
+    return;
+
+  string system_cmd = OPEN_URL_FUNCTION + url;
+  cout << "cmd:[" << system_cmd << "]" << endl;
+}
+
+
+
+
+
+
+
+
---
+++ ivp/src/lib_mbutil/OpenURL.h
@@ -0,0 +1,34 @@
+/*****************************************************************/
+/*    NAME: Michael Benjamin                                     */
+/*    ORGN: Dept of Mechanical Eng / MIT Cambridge MA            */
+/*    FILE: OpenURL.h                                            */
+/*    DATE: Jan 23rd, 2022                                       */
+/*                                                               */
+/* This file is part of IvP Helm Core Libs                       */
+/*                                                               */
+/* IvP Helm Core Libs is free software: you can redistribute it  */
+/* and/or modify it under the terms of the Lesser GNU General    */
+/* Public License as published by the Free Software Foundation,  */
+/* either version 3 of the License, or (at your option) any      */
+/* later version.                                                */
+/*                                                               */
+/* IvP Helm Core Libs is distributed in the hope that it will    */
+/* be useful but WITHOUT ANY WARRANTY; without even the implied  */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the Lesser GNU General Public License for more   */
+/* details.                                                      */
+/*                                                               */
+/* You should have received a copy of the Lesser GNU General     */
+/* Public License along with MOOS-IvP.  If not, see              */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
+
+#ifndef OPEN_URL_HEADER
+#define OPEN_URL_HEADER
+
+#include <string>
+
+void openURL(std::string);
+
+#endif
+
