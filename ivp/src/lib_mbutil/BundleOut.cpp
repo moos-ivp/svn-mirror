75dec575120dc9a63e69261b22eb328e91304cbd
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2022-01-10 01:18:58 +0000

    added BundleOut utilty to mbutil
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@9933 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/lib_mbutil/BundleOut.cpp
@@ -0,0 +1,60 @@
+/*****************************************************************/
+/*    NAME: Michael Benjamin                                     */
+/*    ORGN: Dept of Mechanical Eng / MIT Cambridge MA            */
+/*    FILE: BundleOut.cpp                                        */
+/*    DATE: Jan 9th, 2022                                        */
+/*                                                               */
+/* This file is part of IvP Helm Core Libs                       */
+/*                                                               */
+/* IvP Helm Core Libs is free software: you can redistribute it  */
+/* and/or modify it under the terms of the Lesser GNU General    */
+/* Public License as published by the Free Software Foundation,  */
+/* either version 3 of the License, or (at your option) any      */
+/* later version.                                                */
+/*                                                               */
+/* IvP Helm Core Libs is distributed in the hope that it will    */
+/* be useful but WITHOUT ANY WARRANTY; without even the implied  */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the Lesser GNU General Public License for more   */
+/* details.                                                      */
+/*                                                               */
+/* You should have received a copy of the Lesser GNU General     */
+/* Public License along with MOOS-IvP.  If not, see              */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
+
+#include <iostream>
+#include "BundleOut.h"
+
+using namespace std;
+
+//---------------------------------------------------------
+// Procedure: bundle_cout(list<string>)
+
+void bundle_cout(const list<string>& lines)
+{
+  list<string>::const_iterator p;
+  for(p=lines.begin(); p!=lines.end(); p++)
+    cout << *p << endl;
+}
+
+//---------------------------------------------------------
+// Procedure: bundle_cout(set<string>)
+
+void bundle_cout(const set<string>& lines)
+{
+  set<string>::const_iterator p;
+  for(p=lines.begin(); p!=lines.end(); p++)
+    cout << *p << endl;
+}
+
+//---------------------------------------------------------
+// Procedure: bundle_cout(vector<string>)
+
+void bundle_cout(const vector<string>& lines)
+{
+  vector<string>::const_iterator p;
+  for(p=lines.begin(); p!=lines.end(); p++)
+    cout << *p << endl;
+}
+
---
+++ ivp/src/lib_mbutil/BundleOut.h
@@ -0,0 +1,41 @@
+/*****************************************************************/
+/*    NAME: Michael Benjamin                                     */
+/*    ORGN: Dept of Mechanical Eng / MIT Cambridge MA            */
+/*    FILE: BundleOut.h                                          */
+/*    DATE: Jan 9th, 2022                                        */
+/*                                                               */
+/* This file is part of IvP Helm Core Libs                       */
+/*                                                               */
+/* IvP Helm Core Libs is free software: you can redistribute it  */
+/* and/or modify it under the terms of the Lesser GNU General    */
+/* Public License as published by the Free Software Foundation,  */
+/* either version 3 of the License, or (at your option) any      */
+/* later version.                                                */
+/*                                                               */
+/* IvP Helm Core Libs is distributed in the hope that it will    */
+/* be useful but WITHOUT ANY WARRANTY; without even the implied  */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the Lesser GNU General Public License for more   */
+/* details.                                                      */
+/*                                                               */
+/* You should have received a copy of the Lesser GNU General     */
+/* Public License along with MOOS-IvP.  If not, see              */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
+
+#ifndef BUNDLE_OUT_HEADER
+#define BUNDLE_OUT_HEADER
+
+#include <string>
+#include <list>
+#include <set>
+#include <vector>
+
+void bundle_cout(const std::list<std::string>&);
+
+void bundle_cout(const std::set<std::string>&);
+
+void bundle_cout(const std::vector<std::string>&);
+
+#endif
+
