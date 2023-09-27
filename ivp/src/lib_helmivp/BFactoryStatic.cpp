a0a3e83196622d728d42118eb18a39e5b59dbcde
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2023-09-27 21:19:08 +0000

    rolled back BHV_AvoidObstacleV23
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@10386 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_helmivp/BFactoryStatic.cpp
+++ ivp/src/lib_helmivp/BFactoryStatic.cpp
@@ -61,7 +61,7 @@
 #include "BHV_AvoidObstacle.h"
 #include "BHV_AvoidObstacleX.h"
 #include "BHV_AvoidObstacleV21.h"
-#include "BHV_AvoidObstacleV23.h"
+//#include "BHV_AvoidObstacleV23.h"
 #include "BHV_GoToDepth.h"
 #include "BHV_MemoryTurnLimit.h"
 #include "BHV_Hysteresis.h"
@@ -110,7 +110,7 @@ bool BFactoryStatic::isKnownBehavior(string bhv_name) const
      (bhv_name == "BHV_AvoidObstacle")   || 
      (bhv_name == "BHV_AvoidObstacleX")  || 
      (bhv_name == "BHV_AvoidObstacleV21")|| 
-     (bhv_name == "BHV_AvoidObstacleV23")|| 
+     //     (bhv_name == "BHV_AvoidObstacleV23")|| 
      (bhv_name == "BHV_PeriodicSpeed")   || 
      (bhv_name == "BHV_PeriodicSurface") || 
      (bhv_name == "BHV_GoToDepth")       || 
@@ -199,8 +199,8 @@ IvPBehavior* BFactoryStatic::newBehavior(string bhv_name) const
     bhv = new BHV_AvoidObstacleX(m_domain);
   else if(bhv_name == "BHV_AvoidObstacleV21") 
     bhv = new BHV_AvoidObstacleV21(m_domain);
-  else if(bhv_name == "BHV_AvoidObstacleV23") 
-    bhv = new BHV_AvoidObstacleV23(m_domain);
+  //  else if(bhv_name == "BHV_AvoidObstacleV23") 
+  //   bhv = new BHV_AvoidObstacleV23(m_domain);
   else if(bhv_name == "BHV_PeriodicSpeed") 
     bhv = new BHV_PeriodicSpeed(m_domain);
   else if(bhv_name == "BHV_PeriodicSurface") 
