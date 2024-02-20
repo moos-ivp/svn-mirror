d47e68c3e6e9c78ad38f8d3345ec20603943badd
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2024-02-20 02:23:02 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@10502 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/m4_dingo/launch.sh
+++ ivp/missions/m4_dingo/launch.sh
@@ -2,14 +2,12 @@
 #----------------------------------------------------------
 #  Script: launch.sh
 #  Author: Michael Benjamin
-#  LastEd: Apr 25th 2020
+#  LastEd: Feb 20th 2024
 #----------------------------------------------------------
 #  Part 1: Set Exit actions and declare global var defaults
 #----------------------------------------------------------
-trap "kill -- -$$" EXIT SIGTERM SIGHUP SIGINT SIGKILL
 TIME_WARP=1
 JUST_MAKE="no"
-VARIATION="0"
 
 #-------------------------------------------------------
 #  Part 2: Check for and handle command-line arguments
@@ -22,12 +20,6 @@ for ARGI; do
 	exit 0;
     elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
 	TIME_WARP=$ARGI
-    elif [ "${ARGI}" = "v1" ] ; then
-	VARIATION="1"
-    elif [ "${ARGI}" = "v2" ] ; then
-	VARIATION="2"
-    elif [ "${ARGI}" = "v3" ] ; then
-	VARIATION="3"
     elif [ "${ARGI}" = "--just_make" -o "${ARGI}" = "-j" ] ; then
 	JUST_MAKE="yes"
     else 
@@ -39,82 +31,83 @@ done
 #-------------------------------------------------------
 #  Part 3: Create the .moos and .bhv files. 
 #-------------------------------------------------------
-VNAME1="ella"           # The first  vehicle Community
-VNAME2="frank"           # The second vehicle Community
-VNAME3="gilda"           # The third  vehicle Community
-VNAME4="henry"           # The fourth vehicle Community
+VNAME1="abe"           
+VNAME2="ben"           
+VNAME3="cal"           
+VNAME4="deb"           
 START_POS1="0,0"         
 START_POS2="20,0"        
 START_POS3="40,0"         
 START_POS4="60,50"        
 LOITER_POS1="x=0,y=-75"
 LOITER_POS2="x=125,y=-50"
-LOITER_POS3="x=0,y=-175"
-LOITER_POS4="x=125,y=-150"
-SHORE_LISTEN="9300"
+LOITER_POS3="x=0,y=-125"
+LOITER_POS4="x=125,y=-120"
+SHORE_PSHARE="9200"
 
-nsplug meta_vehicle.moos targ_ella.moos -f WARP=$TIME_WARP \
-    VNAME=$VNAME1          SHARE_LISTEN="9301"              \
-    VPORT="9001"           SHORE_LISTEN=$SHORE_LISTEN       \
-    START_POS=$START_POS1  VARIATION=$VARIATION             
+nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME1          PSHARE_PORT="9201"              \
+    VPORT="9001"           SHORE_PSHARE=$SHORE_PSHARE      \
+    START_POS=$START_POS1 
 
-nsplug meta_vehicle.moos targ_frank.moos -f WARP=$TIME_WARP \
-    VNAME=$VNAME2          SHARE_LISTEN="9302"              \
-    VPORT="9002"           SHORE_LISTEN=$SHORE_LISTEN       \
-    START_POS=$START_POS2  VARIATION=$VARIATION             
+nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME2          PSHARE_PORT="9202"              \
+    VPORT="9002"           SHORE_PSHARE=$SHORE_PSHARE      \
+    START_POS=$START_POS2 
 
 
-nsplug meta_vehicle.moos targ_gilda.moos -f WARP=$TIME_WARP \
-    VNAME=$VNAME3          SHARE_LISTEN="9303"              \
-    VPORT="9003"           SHORE_LISTEN=$SHORE_LISTEN       \
-    START_POS=$START_POS3  VARIATION=$VARIATION             
+nsplug meta_vehicle.moos targ_$VNAME3.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME3          PSHARE_PORT="9203"              \
+    VPORT="9003"           SHORE_PSHARE=$SHORE_PSHARE      \
+    START_POS=$START_POS3 
 
-nsplug meta_vehicle.moos targ_henry.moos -f WARP=$TIME_WARP \
-    VNAME=$VNAME4          SHARE_LISTEN="9304"              \
-    VPORT="9004"           SHORE_LISTEN=$SHORE_LISTEN       \
-    START_POS=$START_POS4  VARIATION=$VARIATION             
+nsplug meta_vehicle.moos targ_$VNAME4.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME4          PSHARE_PORT="9204"              \
+    VPORT="9004"           SHORE_PSHARE=$SHORE_PSHARE      \
+    START_POS=$START_POS4        
 
 nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
-    SNAME="shoreside"  SHARE_LISTEN=$SHORE_LISTEN                 \
-    SPORT="9000"       VARIATION=$VARIATION         
+    MOOS_PORT="9000"       PSHARE_PORT=$SHORE_PSHARE
+    
 
-nsplug meta_vehicle.bhv targ_ella.bhv -f VNAME=$VNAME1     \
+nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1     \
     START_POS=$START_POS1 LOITER_POS=$LOITER_POS1       
 
-nsplug meta_vehicle.bhv targ_frank.bhv -f VNAME=$VNAME2     \
+nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2     \
     START_POS=$START_POS2 LOITER_POS=$LOITER_POS2       
 
-nsplug meta_vehicle.bhv targ_gilda.bhv -f VNAME=$VNAME3     \
+nsplug meta_vehicle.bhv targ_$VNAME3.bhv -f VNAME=$VNAME3     \
     START_POS=$START_POS3 LOITER_POS=$LOITER_POS3       
 
-nsplug meta_vehicle.bhv targ_henry.bhv -f VNAME=$VNAME4     \
+nsplug meta_vehicle.bhv targ_$VNAME4.bhv -f VNAME=$VNAME4     \
     START_POS=$START_POS4 LOITER_POS=$LOITER_POS4       
 
-if [ ${JUST_MAKE} = "yes" ] ; then
+if [ ${JUST_MAKE} = "yes" ]; then
     exit 0
 fi
 
 #-------------------------------------------------------
 #  Part 4: Launch the processes
 #-------------------------------------------------------
-echo "Launching $SNAME MOOS Community with WARP:"  $TIME_WARP
+echo "Launching Shoreside MOOS Community with WARP:"  $TIME_WARP
 pAntler targ_shoreside.moos >& /dev/null &
 sleep 0.25
 
 echo "Launching $VNAME1 MOOS Community with WARP:" $TIME_WARP
-pAntler targ_ella.moos >& /dev/null &
+pAntler targ_$VNAME1.moos >& /dev/null &
 sleep 0.25
 echo "Launching $VNAME2 MOOS Community with WARP:" $TIME_WARP
-pAntler targ_frank.moos >& /dev/null &
+pAntler targ_$VNAME2.moos >& /dev/null &
 
 echo "Launching $VNAME3 MOOS Community with WARP:" $TIME_WARP
-pAntler targ_gilda.moos >& /dev/null &
+pAntler targ_$VNAME3.moos >& /dev/null &
 sleep 0.25
 echo "Launching $VNAME4 MOOS Community with WARP:" $TIME_WARP
-pAntler targ_henry.moos >& /dev/null &
+pAntler targ_$VNAME4.moos >& /dev/null &
 
 echo "Done."
 
 uMAC targ_shoreside.moos
 
+kill -- -$$
 
