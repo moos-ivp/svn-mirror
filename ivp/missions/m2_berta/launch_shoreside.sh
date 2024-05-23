a3a6e7c8670ee97a94f1981351191bb6951e4027
mikerb <mikerb@7b6de600-d9ab-45c9-a1ca-e8145d3c6daf>
2024-05-23 19:03:25 +0000

    mods to m2_berta
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@10575 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/m2_berta/launch_shoreside.sh
+++ ivp/missions/m2_berta/launch_shoreside.sh
@@ -1,18 +1,18 @@
 #!/bin/bash 
-#-------------------------------------------------------------- 
+#------------------------------------------------------------ 
 #   Script: launch_shoreside.sh    
 #  Mission: m2_berta
 #   Author: M.Benjamin   
 #   LastEd: May 2024       
-#-------------------------------------------------------------- 
+#------------------------------------------------------------ 
 #  Part 1: A convenience function for producing terminal
 #          debugging/status output depending on verbosity.
-#-------------------------------------------------------------- 
+#------------------------------------------------------------ 
 vecho() { if [ "$VERBOSE" != "" ]; then echo "$ME: $1"; fi }
 
-#-------------------------------------------------------------- 
-#  Part 1: Set global variable
-#-------------------------------------------------------------- 
+#------------------------------------------------------------ 
+#  Part 2: Set global variable default values
+#------------------------------------------------------------ 
 ME=`basename "$0"`
 TIME_WARP=1
 JUST_MAKE=""
@@ -24,36 +24,36 @@ LAUNCH_GUI="yes"
 IP_ADDR="localhost"
 MOOS_PORT="9000"
 PSHARE_PORT="9200"
+
 VNAMES="abe:ben"
 
-#--------------------------------------------------------------
-#  Part 2: Check for and handle command-line arguments
-#--------------------------------------------------------------
+#------------------------------------------------------------ 
+#  Part 3: Check for and handle command-line arguments
+#------------------------------------------------------------ 
 for ARGI; do
     CMD_ARGS+="${ARGI} "
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ]; then
-	echo "$ME: [OPTIONS] [time_warp]                       " 
-	echo "                                                 "
-	echo "Options:                                         "
-        echo "  --help, -h                                     "
-        echo "    Display this help message                    "
-        echo "  --verbose, -v                                  "
-        echo "    Increase verbosity                           "
-	echo "  --just_make, -j                                " 
-	echo "    Just make target files. Do not launch.       "
-        echo "  --auto, -a                                     "
-        echo "    Auto-launched by a script.                  "
-        echo "    Will not launch uMAC as the final step.     "
-        echo "  --nogui, -n                                    "
-        echo "    Headless mission, no pMarineViewer           "
-	echo "                                                 "
-	echo "  --ip=<localhost>                               " 
-	echo "    Force pHostInfo to use this IP Address       "
-	echo "  --mport=<9000>                                 "
-	echo "    Port number of this vehicle's MOOSDB port    "
-	echo "  --pshare=<9200>                                " 
-	echo "    Port number of this vehicle's pShare port    "
-	echo "                                                 "
+	echo "$ME: [OPTIONS] [time_warp]                     " 
+	echo "                                               "
+	echo "Options:                                       "
+	echo "  --help, -h         Show this help message    " 
+	echo "  --just_make, -j    Only create targ files    " 
+	echo "  --verbose, -v      Verbose, confirm launch   "
+	echo "                                               "
+        echo "  --auto, -a                                   "
+        echo "    Auto-launched by a script.                 "
+        echo "    Will not launch uMAC as the final step.    "
+        echo "  --nogui, -n                                  "
+        echo "    Headless mode - no pMarineViewer etc       "
+	echo "                                               "
+	echo "  --ip=<localhost>                             "
+	echo "    Force pHostInfo to use this IP Address     "
+	echo "  --mport=<9000>                               "
+	echo "    Port number of this vehicle's MOOSDB port  "
+	echo "  --pshare=<9200>                              "
+	echo "    Port number of this vehicle's pShare port  "
+        echo "  --vnames=<vnames>                            "
+        echo "    Colon-separate list of all vehicle names   "
 	exit 0;
     elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
         TIME_WARP=$ARGI
@@ -74,18 +74,17 @@ for ARGI; do
         PSHARE_PORT="${ARGI#--pshare=*}"
     elif [ "${ARGI:0:9}" = "--vnames=" ]; then
         VNAMES="${ARGI#--vnames=*}"
-	
-    else 
-	echo "$ME: Bad Arg: $ARGI. Exit Code 1."
+    else
+	echo "$ME: Bad Arg:[$ARGI]. Exit Code 1."
 	exit 1
     fi
 done
 
-#---------------------------------------------------------------
-#  Part 3: If not auto_launched (likely running in the field),
+#------------------------------------------------------------ 
+#  Part 4: If not auto_launched (likely running in the field),
 #          and the IP_ADDR has not be explicitly set, try to get
 #          it using the ipaddrs.sh script. 
-#---------------------------------------------------------------
+#------------------------------------------------------------ 
 if [ "${AUTO_LAUNCHED}" = "no" -a "${IP_ADDR}" = "localhost" ]; then
     MAYBE_IP_ADDR=`ipaddrs.sh --blunt`
     if [ $? = 0 ]; then
@@ -93,18 +92,18 @@ if [ "${AUTO_LAUNCHED}" = "no" -a "${IP_ADDR}" = "localhost" ]; then
     fi
 fi
 
-#---------------------------------------------------------------
-#  Part 4: If verbose, show vars and confirm before launching
-#---------------------------------------------------------------
+#------------------------------------------------------------ 
+#  Part 5: If verbose, show vars and confirm before launching
+#------------------------------------------------------------ 
 if [ "${VERBOSE}" = "yes" ]; then 
     echo "=================================="
     echo "  launch_shoreside.sh SUMMARY     "
     echo "=================================="
-    echo "$ME"
+    echo "$ME                               "
     echo "CMD_ARGS =      [${CMD_ARGS}]     "
     echo "TIME_WARP =     [${TIME_WARP}]    "
-    echo "AUTO_LAUNCHED = [${AUTO_LAUNCHED}]"
     echo "JUST_MAKE =     [${JUST_MAKE}]    "
+    echo "AUTO_LAUNCHED = [${AUTO_LAUNCHED}]"
     echo "----------------------------------"
     echo "IP_ADDR =       [${IP_ADDR}]      "
     echo "MOOS_PORT =     [${MOOS_PORT}]    "
@@ -116,41 +115,40 @@ if [ "${VERBOSE}" = "yes" ]; then
     read ANSWER
 fi
 
-#-------------------------------------------------------
-#  Part 5: Create the .moos and .bhv files. 
-#-------------------------------------------------------
-NSFLAGS="-s -f"
+#------------------------------------------------------------ 
+#  Part 6: Create the shoreside mission file
+#------------------------------------------------------------ 
+NSFLAGS="--strict --force"
 if [ "${AUTO_LAUNCHED}" = "no" ]; then
-    NSFLAGS="-i -f"
+    NSFLAGS="--interactive --force"
 fi
 
 nsplug meta_shoreside.moos targ_shoreside.moos $NSFLAGS WARP=$TIME_WARP \
-       IP_ADDR=$IP_ADDR       PSHARE_PORT=$PSHARE_PORT     \
-       MOOS_PORT=$MOOS_PORT   LAUNCH_GUI=$LAUNCH_GUI       \
-       SWIM_FILE=$SWIM_FILE   VNAMES=$VNAMES
+       IP_ADDR=$IP_ADDR             MOOS_PORT=$MOOS_PORT    \
+       PSHARE_PORT=$PSHARE_PORT     LAUNCH_GUI=$LAUNCH_GUI  \
+       VNAMES=$VNAMES
 
 if [ "${JUST_MAKE}" = "yes" ]; then
     echo "Targ files made; exiting without launch."
     exit 0
 fi
 
-#-------------------------------------------------------
-#  Part 6: Launch the processes
-#-------------------------------------------------------
+#------------------------------------------------------------ 
+#  Part 7: Launch the shoreside MOOS community
+#------------------------------------------------------------ 
 echo "Launching Shoreside MOOS Community. WARP="$TIME_WARP
 pAntler targ_shoreside.moos >& /dev/null &
 echo "Done Launching Shoreside Community"
 
-
-#-------------------------------------------------------
-#  Part 7: If launched from script, we're done, exit now
-#-------------------------------------------------------
+#------------------------------------------------------------ 
+#  Part 8: If launched from script, we're done, exit now
+#------------------------------------------------------------ 
 if [ "${AUTO_LAUNCHED}" = "yes" ]; then
     exit 0
 fi
 
-#-------------------------------------------------------
-# Part 8: Launch uMAC until the mission is quit
-#-------------------------------------------------------
+#------------------------------------------------------------ 
+# Part 9: Launch uMAC until the mission is quit
+#------------------------------------------------------------ 
 uMAC targ_shoreside.moos
 kill -- -$$
